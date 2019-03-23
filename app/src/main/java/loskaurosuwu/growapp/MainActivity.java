b64e911f54d0583839380f9df034de19e12ccb72
Alexander Irribarra <airribarra@udec.cl>
2019-03-23 15:08:31 -0300

    aaaa

--- app/src/main/java/loskaurosuwu/growapp/MainActivity.java
+++ app/src/main/java/loskaurosuwu/growapp/MainActivity.java
@@ -1,8 +1,11 @@
 package loskaurosuwu.growapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.v("esto es una prueba","ola ola");
-        setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_main);
+        Button catalogo = findViewById(R.id.catalogbutton);
+        catalogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                catalogButtonClick();
+            }
+        });
 
+    }
+    public void catalogButtonClick(){
+        Log.v("presione","catalog");
+        Intent i = new Intent(this,CatalogoPlantas.class);
+        startActivity(i);
     }
 }
