e2691de0aa7fd9fa39831607521e58c8f6100724
Alexander Irribarra <airribarra@udec.cl>
2019-03-23 13:15:34 -0300

    aaaa

--- app/src/main/java/loskaurosuwu/growapp/MainActivity.java
+++ app/src/main/java/loskaurosuwu/growapp/MainActivity.java
@@ -2,12 +2,14 @@ package loskaurosuwu.growapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("esto es una prueba","ola ola");
         setContentView(R.layout.activity_main);
     }
 }
